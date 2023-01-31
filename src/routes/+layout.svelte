511c8707f39e0485fabd7307b85da78760e2907a
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-01 05:18:43 +0800

    -

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script>
+  import "../app.css";
+</script>
+<nav class="bg-blue-900 p-4">
+  <div class="container flex">
+    <h1 class="m-0 text-lg text-amber-400 font-bold">Link Start</h1>
+    <ul class="flex m-0 ml-auto list-none text-base">
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/services">Services</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+  <h1 class="text-3xl font-bold underline">
+    <slot />
+  </h1>
+</div>
+
+<style>
+  li {
+    margin-right: 20px;
+  }
+  a {
+    text-decoration: none;
+    color: aliceblue;
+  }
+</style>

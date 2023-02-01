48215edb05231b6b98c7ed3e2bc3aafb0b86ebc9
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-01 14:06:23 +0800

    improveing navs

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -2,25 +2,37 @@
   import "../app.css";
   import { page } from "$app/stores";
 
+  const navs = [
+    {
+      title: "Home",
+      href: "/"
+    },
+    {
+      title: "About",
+      href: "/about"
+    },
+    {
+      title: "Services",
+      href: "/services"
+    },
+    {
+      title: "Contact",
+      href: "/contact"
+    },
+  ];
+
   $: routeId = $page.route.id;
 </script>
 
 <nav class="bg-blue-900 p-4">
-  <div class="container flex">
+  <div class="container flex items-center">
     <h1 class="m-0 text-lg text-amber-400 font-bold">Link Start</h1>
     <ul class="flex m-0 ml-auto list-none text-base">
+      {#each navs as { title, href }}
       <li>
-        <a href="/" class:active={routeId == "/"}>Home</a>
+        <a {href} class:active={routeId == href}>{title}</a>
       </li>
-      <li>
-        <a href="/about" class:active={routeId == "/about"}>About</a>
-      </li>
-      <li>
-        <a href="/services" class:active={routeId == "/services"}>Services</a>
-      </li>
-      <li>
-        <a href="/contact" class:active={routeId == "/contact"}>Contact</a>
-      </li>
+      {/each}
     </ul>
   </div>
 </nav>

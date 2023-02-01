e81aa6d23a4f687754707d88d99cc96d2c7b758f
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-01 13:56:43 +0800

    some addition

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,21 +1,25 @@
 <script>
   import "../app.css";
+  import { page } from "$app/stores";
+
+  $: routeId = $page.route.id;
 </script>
+
 <nav class="bg-blue-900 p-4">
   <div class="container flex">
     <h1 class="m-0 text-lg text-amber-400 font-bold">Link Start</h1>
     <ul class="flex m-0 ml-auto list-none text-base">
       <li>
-        <a href="/">Home</a>
+        <a href="/" class:active={routeId == "/"}>Home</a>
       </li>
       <li>
-        <a href="/about">About</a>
+        <a href="/about" class:active={routeId == "/about"}>About</a>
       </li>
       <li>
-        <a href="/services">Services</a>
+        <a href="/services" class:active={routeId == "/services"}>Services</a>
       </li>
       <li>
-        <a href="/contact">Contact</a>
+        <a href="/contact" class:active={routeId == "/contact"}>Contact</a>
       </li>
     </ul>
   </div>
@@ -29,5 +33,9 @@
   a {
     text-decoration: none;
     color: aliceblue;
+  }
+  .active {
+    color: azure;
+    font-weight: 900;
   }
 </style>

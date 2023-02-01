412c02fc08a7c2cbc32cf437396f86fb70254990
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-01 13:43:13 +0800

    some change

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,33 @@
+<script>
+  import "../app.css";
+</script>
+<nav class="bg-blue-900 p-4">
+  <div class="container flex">
+    <h1 class="m-0 text-lg text-amber-400 font-bold">Link Start</h1>
+    <ul class="flex m-0 ml-auto list-none text-base">
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/services">Services</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+<style>
+  li {
+    margin-right: 20px;
+  }
+  a {
+    text-decoration: none;
+    color: aliceblue;
+  }
+</style>

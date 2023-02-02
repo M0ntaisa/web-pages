be6df2b81773ff888fb9976c242ea0d588c7a4e5
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-02 18:51:17 +0800

    fetch api for data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,33 @@
 <script>
-
+	import { onMount } from "svelte";
 	import Hero from "./Hero.svelte";
 	import What from "./What.svelte";
+
+	/**
+	 * @type {any[]}
+	 */
+	let posts = []
+	onMount(async() => {
+		posts = await getPosts();
+	})
+
+	const getPosts = async() => {
+		const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+		const data = await res.json();
+		const filteredData = data.slice(0, 5);
+		return filteredData;
+	}
 
 </script>
 
 <Hero />
 <What />
+
+<div class="container grid grid-cols-3 gap-3 my-5">
+	{#each posts as { title, url} }
+		<div class="p-2">
+			<img src={url} alt={title} />
+			<p>{title}</p>
+		</div>
+	{/each}
+</div>

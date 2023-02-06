d3d0494e28a013b9ed8493dd0d791ad8e581aed5
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-06 14:34:59 +0800

    add services

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,1 +1,13 @@
-Services
+<div class="container">
+  <h2 class="font-bold text-2xl text-center p-3">Our Services</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eveniet expedita ducimus, corrupti quod nesciunt commodi maiores dolorem error totam ipsum aut temporibus a enim in. Aliquam dolore recusandae a adipisci. Aliquid necessitatibus rerum veritatis suscipit obcaecati et excepturi velit numquam eius laboriosam, ea placeat maxime quae mollitia distinctio sint!</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa fugiat dolore eveniet repellendus cupiditate consectetur temporibus! Cumque, reprehenderit molestiae deleniti modi ratione, itaque impedit est soluta, delectus nam id ipsa!</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe non facere impedit, perspiciatis nesciunt possimus eligendi assumenda maiores rem nulla odio dicta ea ipsam neque, in optio dolorem amet laudantium, hic nostrum esse. Autem placeat a quam ipsum dicta illum vero sunt iure repellat in voluptatem, nemo esse aperiam nesciunt incidunt non labore nulla qui reiciendis nam! Cupiditate iste, nihil quod at obcaecati laudantium tempora illo aliquid rerum, expedita enim!</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias temporibus pariatur suscipit, deserunt laboriosam provident nihil, unde assumenda corrupti rem cumque recusandae vero architecto quo illo delectus molestiae ipsa sapiente cum, aperiam omnis. Facilis illo totam dicta accusamus optio eaque atque cumque unde dolore sunt. Dolores voluptate distinctio provident ipsam eos dolore nemo reiciendis iusto quae. Omnis, obcaecati delectus? Quibusdam, non. Aperiam veniam quo ducimus. Cumque est blanditiis aliquam sequi itaque non, reiciendis reprehenderit doloribus ad minus dolores similique, error provident alias ut ab dolore quidem amet exercitationem porro nulla tempore optio rem. Sequi unde voluptates quam ipsum repellat ut!</p>
+</div>
+
+<style>
+  p {
+    padding: 0.5em 0;
+  }
+</style>

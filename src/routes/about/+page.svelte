6170b51e82a3ec6141563e3416a46268daa08987
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-07 17:08:58 +0800

    update about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,11 @@
-About
+<div class="container">
+  <h2 class="font-bold text-2xl text-center p-3">About Us</h2>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa fugiat dolore eveniet repellendus cupiditate consectetur temporibus! Cumque, reprehenderit molestiae deleniti modi ratione, itaque impedit est soluta, delectus nam id ipsa!</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias temporibus pariatur suscipit, deserunt laboriosam provident nihil, unde assumenda corrupti rem cumque recusandae vero architecto quo illo delectus molestiae ipsa sapiente cum, aperiam omnis. Facilis illo totam dicta accusamus optio eaque atque cumque unde dolore sunt. Dolores voluptate distinctio provident ipsam eos dolore nemo reiciendis iusto quae. Omnis, obcaecati delectus? Quibusdam, non. Aperiam veniam quo ducimus. Cumque est blanditiis aliquam sequi itaque non, reiciendis reprehenderit doloribus ad minus dolores similique, error provident alias ut ab dolore quidem amet exercitationem porro nulla tempore optio rem. Sequi unde voluptates quam ipsum repellat ut!</p>
+</div>
+
+<style>
+  p {
+    padding: 0.5em 0;
+  }
+</style>

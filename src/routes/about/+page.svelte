cc01e1ba52ce6e94c793d2af80e051d0ca7fb0e3
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-07 22:30:42 +0800

    some addition

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,8 @@
 <div class="container">
   <h2 class="font-bold text-2xl text-center p-3">About Us</h2>
+  <div class="p-2 w-3/4">
+    <img src="#" alt="img" />
+  </div>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa fugiat dolore eveniet repellendus cupiditate consectetur temporibus! Cumque, reprehenderit molestiae deleniti modi ratione, itaque impedit est soluta, delectus nam id ipsa!</p>
   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias temporibus pariatur suscipit, deserunt laboriosam provident nihil, unde assumenda corrupti rem cumque recusandae vero architecto quo illo delectus molestiae ipsa sapiente cum, aperiam omnis. Facilis illo totam dicta accusamus optio eaque atque cumque unde dolore sunt. Dolores voluptate distinctio provident ipsam eos dolore nemo reiciendis iusto quae. Omnis, obcaecati delectus? Quibusdam, non. Aperiam veniam quo ducimus. Cumque est blanditiis aliquam sequi itaque non, reiciendis reprehenderit doloribus ad minus dolores similique, error provident alias ut ab dolore quidem amet exercitationem porro nulla tempore optio rem. Sequi unde voluptates quam ipsum repellat ut!</p>
 </div>

99febf881bad55687c73e5d20381ee1ad6b350b7
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-07 22:32:45 +0800

    some change

--- src/routes/Posts.svelte
+++ src/routes/Posts.svelte
@@ -7,7 +7,7 @@
 
 <div class="container grid grid-cols-3 gap-3 my-5">
 	{#each posts as { title, url} }
-		<div class="p-2 w-3/4">
+		<div class="p-2 w-1/3">
 			<img src={url} alt={title} />
 			<p>{title}</p>
 		</div>

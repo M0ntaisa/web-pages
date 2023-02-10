c860dc212a791e6568b9a1ad4c60452ca552ac14
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-10 09:08:23 +0800

    using yup for validate

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -1,49 +1,34 @@
 import { fail } from "@sveltejs/kit";
+import { object, string, number, date, InferType } from 'yup';
 
 export const actions = {
-  default: async({request}) => {
+  default: async({request}) => {  
     const formData = await request.formData();
     const name = formData.get("name");
     const email = formData.get("email");
     const message = formData.get("message");
 
+    const contactFormSchema = object({
+      name: string().required(),
+      email: string().required().email(),
+      message: string().required()
+    });
+
     // do think with data
 
-    const ERROR_MESSAGES = {
-      NAME_MISSING: "Name is missing",
-      EMAIL_MISSING: "Email is missing",
-      MESSAGE_MISSING: "Message is missing",
-    };
-
-    const validateForm = (name, email, message) => {
-      const missingField = [];
-
-      if (!name) {
-        missingField.push(ERROR_MESSAGES.NAME_MISSING);
+    try {
+      const result = await contactFormSchema.validate({ name, email, message });
+      console.log(result);
+      return {
+        success: true,
+        status: "Form is submitted"
       }
-      if (!email) {
-        missingField.push(ERROR_MESSAGES.EMAIL_MISSING);
-      }
-      if (!message) {
-        missingField.push(ERROR_MESSAGES.MESSAGE_MISSING);
-      }
+    } catch (error) {
+      console.log(error);
+    }
 
-      if (missingField.length) {
-        return fail(400, {
-          message: `${missingField.join(", ")}`,
-        })
-      } else {
-        return {
-          success: true,
-          message: "Form is submitted",
-        };
-      }
-    } 
 
-    console.log(validateForm(name, email, message));
-
-    return validateForm(name, email, message);
-
+    
     
   }
 }

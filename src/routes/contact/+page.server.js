32229c1f0826ef3ffd26961c70263369f172b06c
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-13 18:11:16 +0800

    send form response into Gform

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -19,10 +19,17 @@ export const actions = {
     try {
       const result = await contactFormSchema.validate({ name, email, message }, { abortEarly: false });
       console.log(result);
+      
+      const prefilledLink = `https://docs.google.com/forms/d/e/1FAIpQLScaSr2ahQ6CqhP6FVnR1t1MFJvwfCwCBQPiyX8oN71cT2_FDQ/formResponse?usp=pp_url&entry.642516482=${name}&entry.2063620975=${email}&entry.1446559721=${message}&submit=Submit`;
+
+      const res = await fetch(prefilledLink);
+      // still not have error handling for the response
+      
       return {
         success: true,
         status: "Form is submitted"
       }
+
     } catch (error) {
       console.log(error);
       const errors = error.inner.reduce((acc, err) => {

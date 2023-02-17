1b3d2cfd32300bb393c17fdc0d3bb009e2a16e7a
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-17 11:34:16 +0800

    add blogs

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,3 +1,21 @@
+<script>
+  export let data;
+  const {posts} = data;
+</script>
+
 <div class="container">
-  Blog
+  <h2 class="font-bold text-2xl mb-2">Blog Posts</h2>
+  <ul>
+    {#each posts as post }
+      <li class="mb-2">
+        <a href="blog/{post.id}">{post.title}</a>
+      </li>
+    {/each}
+  </ul>
 </div>
+
+<style>
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let data;
+  const {post} = data;
+</script>
+
+<div class="container">
+  <h1 class="font-bold text-2xl mb-2">{post.title}</h1>
+  <p class="mb-2">{post.body}</p>
+</div>

eea636e2ede5b372f37ee201a0fec48b628563b7
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-23 22:38:11 +0800

    some updated

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,4 +1,4 @@
-<div class="bg-slate-300 text-slate-900 py-20 px-0">
+<div class="hero bg-slate-300 text-slate-900 py-20 px-0">
   <div class="container">
     <h2 class="m-0 font-bold text-2xl pb-2">DAMN GOOD DAY</h2>
     <p>
@@ -6,3 +6,10 @@
     </p>
   </div>
 </div>
+
+<style>
+  .hero {
+    background-color: var(--hero-bgc);
+    color: var(--color);
+  }
+</style>

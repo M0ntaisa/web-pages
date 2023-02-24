974793bee72aeb52f43c0f8a1b404ead5ff3096f
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-24 09:18:37 +0800

    protected route

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,15 @@
+// import { redirect } from '@sveltejs/kit';
+
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = async({ event, resolve }) => {
+  
+  // console.log(event.route.id?.startsWith("/(app)"))
+  // if (event.route.id?.startsWith("/(app)")) {
+  //   throw redirect(302, "/login");
+  // }
+  
   const theme = event.cookies.get("siteTheme");
-
+  
   const response = await resolve(event, {
     transformPageChunk: ({html}) => html.replace('data-theme=""', `data-theme="${theme}"`)
   });

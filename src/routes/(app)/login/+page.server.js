bdda59de7bd281df5e6c07912019756c02c0807c
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-28 22:23:11 +0800

    auth

--- src/routes/(app)/login/+page.server.js
+++ src/routes/(app)/login/+page.server.js
@@ -1,6 +1,16 @@
+import { redirect } from '@sveltejs/kit';
+
 /** @type {import('./$types').Actions} */
 export const actions = {
-  default: async ({ cookies }) => {
-    cookies.set("access", "true");
+  default: async ({ cookies, request }) => {
+    const formData = await request.formData();
+    const username = formData.get("username");
+    const password = formData.get("password");
+    
+    if (username === "admin" && password === "admin") {
+      cookies.set("access", "true", { path: "/", sameSite: "strict" });
+      throw redirect(302, "/dashboard");
+    } 
+
   },
 };

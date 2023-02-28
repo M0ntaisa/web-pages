18eefd28a82c706926230a299692ae698625753d
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-28 19:59:06 +0800

    updated

--- src/routes/(app)/login/+page.svelte
+++ src/routes/(app)/login/+page.svelte
@@ -1,23 +1,27 @@
+<script>
+
+	import Button from "$lib/Button.svelte";
+
+</script>
+
 <div class="container">
-  <div class="bg-gray-800 h-screen flex flex-col justify-center items-center">
-    <form class="bg-gray-100 rounded-lg px-8 pt-6 pb-8 mb-4">
+  <div class="form h-screen flex flex-col justify-center items-center">
+    <form class="bg-gray-100 rounded-lg px-8 pt-6 pb-8 mb-4" method="POST">
       <h2 class="text-center text-3xl text-gray-800 mb-8 font-bold">Login</h2>
       <div class="mb-4">
         <label class="block text-gray-700 font-bold mb-2" for="username">
           Username
         </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your username">
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="example123">
       </div>
       <div class="mb-4">
         <label class="block text-gray-700 font-bold mb-2" for="password">
           Password
         </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your password">
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="Enter your password">
       </div>
       <div class="flex items-center justify-between">
-        <button class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-          Sign In
-        </button>
+        <Button id="action">Sign In</Button>
       </div>
     </form>
   </div>

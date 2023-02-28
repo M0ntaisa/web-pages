e48bfc993d73a9bd72dce087d844a3e130b234e2
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-02-28 19:23:14 +0800

    add login form

--- src/routes/(app)/login/+page.svelte
+++ src/routes/(app)/login/+page.svelte
@@ -1,7 +1,25 @@
 <div class="container">
-  Login
-
-  <form method="POST">
-    <button>Gain Access</button>
-  </form>
+  <div class="bg-gray-800 h-screen flex flex-col justify-center items-center">
+    <form class="bg-gray-100 rounded-lg px-8 pt-6 pb-8 mb-4">
+      <h2 class="text-center text-3xl text-gray-800 mb-8 font-bold">Login</h2>
+      <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2" for="username">
+          Username
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your username">
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2" for="password">
+          Password
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your password">
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          Sign In
+        </button>
+      </div>
+    </form>
+  </div>
+    
 </div>
